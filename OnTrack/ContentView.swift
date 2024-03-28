81826941f049dd8756bcb93b9b6e6421fba426c6
eamevo <eamevo3@gatech.edu>
2024-03-28 17:50:00 -0400

    Initial Commit

---
+++ OnTrack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnTrack
+//
+//  Created by Enam Amevo on 3/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnTrack/OnTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnTrackApp.swift
+//  OnTrack
+//
+//  Created by Enam Amevo on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct OnTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
