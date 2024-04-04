de9bb832b489f3b24be6c0c9d15c433ac1f0461c
eamevo <150758537+eamevo@users.noreply.github.com>
2024-04-04 18:42:43 -0400

    Update RoutineStruct.swift

--- OnTrack/RoutineStruct.swift
+++ OnTrack/RoutineStruct.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+struct routineStruct: CustomStringConvertible{
+    let name:String
+    let routineDescription:String
+    var tasks: [Task]
+    
+    var description: String{
+        "Routine: \(name), Description: \(routineDescription), Tasks: \(tasks)"
+    }
+}
+
+struct Task{
+    let name:String
+    var completed:Bool
+  //figuring out a way to use completed bool
+        
+    var description: String{"Task: \(name), Completed? \(completed) "}
+    }
+}
+
+
+var routines:[routineStruct]=[]
+
+let routine1 = routineStruct(name: "Test Routine", routineDescription: "Testing 123", tasks: [Task(name: "make bed",completed: false),Task(name: "feed pet",completed: false),Task(name:"affirmations",completed:false)])
