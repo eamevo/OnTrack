0dea018f0cdc6bc86a6939afddaf51ede1702127
ananyapshetty1 <148728759+ananyapshetty1@users.noreply.github.com>
2024-04-09 16:31:54 -0400

    Skeleton Code ContentView.swift
    I am hoping that this works - code is written assuming that functions are implemented in ViewModel

--- OnTrack/ContentView.swift
+++ OnTrack/ContentView.swift
@@ -7,22 +7,47 @@
 //
 //  Created by Enam Amevo on 3/28/24.
 //
-//  Contributors: 
+//  Contributors:
 //
-
+    
 import SwiftUI
 
 struct ContentView: View {
     @EnvironmentObject var vm: ViewModel
+    var numComplete: Int = 0
     
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, \(vm.getDailyQuote())")
+                .padding()
+            
+            Text(vm.getDailyQuote())
+                .padding()
+            
+            List(vm.routines) { routine in
+                HStack {
+                    Button(action: vm.markRoutine()) {
+                        Image(systemName: routine.isCompleted ? "checkmark.square" : "square")
+                    }
+                    Spacer()
+                    VStack {
+                        Text(routine.name)
+                        Text(routine.description)
+                    }
+                }
+            }
+            
+            Button(action: vm.addRoutine()) {
+                Text("Add Routine")
+            }
+            .padding()
+            .onAppear {
+                vm.loadFromStorage()
+                
+                Text("\(numComplete)/\(vm.routines.count)")
+                .padding()
+            }
         }
-        .padding()
     }
 }
 
