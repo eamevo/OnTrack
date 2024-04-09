2b2b1a129c5a90e9d6b2d19feeba2e7425480c4a
eamevo <150758537+eamevo@users.noreply.github.com>
2024-04-09 00:34:30 -0400

    Update Routine.swift
    explained my old code

--- OnTrack/Routine.swift
+++ OnTrack/Routine.swift
@@ -28,22 +28,32 @@ struct Routine: Identifiable {
     var currentStreak: Int
 }
 
-//struct routineStruct: CustomStringConvertible{
-//    let name:String
-//    let routineDescription:String
-//    var tasks: [Task]
-//    
-//    var description: String{
-//        "Routine: \(name), Description: \(routineDescription), Tasks: \(tasks)"
-//    }
-//}
+// code from before the github was updated, kept it incase there's anything useful here.
+// import Foundation
 
-//struct Task: CustomStringConvertible{
-//    let name:String
-//    var completed:Bool
-//    mutating func updateyesOrNo(){
-//        if completed == true{
-//            Bool = "Yes"}
-//        else if completed == false{
-//            yesOrNo = "No"
-//        }
+// struct routineStruct: CustomStringConvertible{
+//     let name:String
+//     let routineDescription:String
+//     var tasks: [Task]
+
+//     var description: String{
+//         "Routine: \(name), Description: \(routineDescription), Tasks: \(tasks)"
+//     }
+// }
+
+// struct Task: CustomStringConvertible{
+//     let name:String
+//     var completed:Bool
+// //    mutating func updateyesOrNo(){
+// //        if completed == true{
+// //            Bool = "Yes"}
+// //        else if completed == false{
+// //            yesOrNo = "No"
+// //        }
+
+//     var description: String{"Task: \(name), Completed? \(completed) "}
+//     }
+
+// var routines:[routineStruct]=[]
+
+// let routine1 = routineStruct(name: "Test Routine", routineDescription: "Testing 123", tasks: [Task(name: "make bed",completed: false),Task(name: "feed pet",completed: false),Task(name:"affirmations",completed:false)])
