57fec040bcb1944117e9ebc7fc98364a577b6e86
ishika-dpatel <ishikapatel.d@gmail.com>
2024-04-09 21:02:35 -0400

    made a skeleton layout for the onBoarding View

--- OnTrack/OnboardingView.swift
+++ OnTrack/OnboardingView.swift
@@ -1,24 +1,57 @@
-//
-//  OnboardingView.swift
-//  OnTrack
-//
-//  Handles first user experience after installation.
-//
-//  Created by Noah Sadir on 4/4/24.
-//
-//  Contributors:
-//
-
 import SwiftUI
 
 struct OnboardingView: View {
-    @EnvironmentObject var vm: ViewModel
-    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            
+            Image("OnTrackLogo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200, height: 100)
+            
+            Text("Ready to get started?")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding()
+            
+            Button(action: {
+                // Action when "Yes!" button tapped
+            }) {
+                Text("Yes!")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            .padding()
+            
+            Spacer()
+            
+            HStack(spacing: 20) {
+                BottomButton(imageName: "calendar")
+                BottomButton(imageName: "home")
+                BottomButton(imageName: "target")
+            }
+            .padding()
+        }
     }
 }
 
-#Preview {
-    OnboardingView()
+struct BottomButton: View {
+    var imageName: String
+    
+    var body: some View {
+        Button(action: {
+            // Action when bottom button tapped
+        }) {
+            Image(imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+        }
+    }
 }
+
