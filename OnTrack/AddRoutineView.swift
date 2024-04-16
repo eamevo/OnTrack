732b67f9639a732dbc0a8c05dafba5e18e469b4c
David <137850658+davidchan0@users.noreply.github.com>
2024-04-16 17:27:00 -0400

    Edited text

--- OnTrack/AddRoutineView.swift
+++ OnTrack/AddRoutineView.swift
@@ -15,7 +15,7 @@ struct AddRoutineView: View {
     @EnvironmentObject var vm: ViewModel
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hi")
     }
 }
 
