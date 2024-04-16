e2d14ce8bce9817e9d2a59a261ed8bd02647a1e1
ananyapshetty1 <148728759+ananyapshetty1@users.noreply.github.com>
2024-04-16 17:15:46 -0400

    Update AddRoutineView.swift
    completed add routine other than frequency stuff -- unsure about that

--- OnTrack/AddRoutineView.swift
+++ OnTrack/AddRoutineView.swift
@@ -13,11 +13,50 @@ import SwiftUI
 
 struct AddRoutineView: View {
     @EnvironmentObject var vm: ViewModel
-    
+    var title = ""
+    var description = ""
+    var frequency = 0
+    let frequencies = ["Daily", "Weekly", "Monthly"]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Form {
+                Section(header: Text("Title")) {
+                    TextField("Enter title", text: $title)
+                }
+
+                Section(header: Text("Description")) {
+                    TextField("Enter description", text: $description)
+                }
+
+                //Text(frequencies[index])
+                //Unsure how to make a dropdown thing
+
+                Section {
+                    Button("Add Routine") {
+                        let id = vm.generateUniqueId()
+                        let frequencyInSeconds = getFrequency(frequency)
+                        let newRoutine = Routine(id: id, title: title, description: description, frequency: frequencyInSeconds, successfulDays: [], currentStreak: 0)
+                        vm.addRoutine(routine: newRoutine)
+                    }
+                }
+            }
+        }
+    }
+
+    func getFrequency(_ frequency: Int) -> Int {
+        switch frequency {
+        case 0:
+            return 0
+        case 1:
+            return 1
+        case 2:
+            return 2
+        default:
+            return 0
+        }
     }
 }
+
 
 #Preview {
     AddRoutineView()
